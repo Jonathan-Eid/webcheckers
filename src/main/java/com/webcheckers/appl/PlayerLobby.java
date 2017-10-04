69d5502e4e2943e9669b5cb13ec435ef905bcd55
txg9879 <txg9879@rit.edu>
2017-10-04 16:45:23 -0400

    Create New Stub classes.

---
+++ src/main/java/com/webcheckers/appl/PlayerLobby.java
@@ -0,0 +1,5 @@
+package com.webcheckers.appl;
+
+public class PlayerLobby {
+    //TODO Handle Sign in and sign out
+}
---
+++ src/main/java/com/webcheckers/model/Player.java
@@ -0,0 +1,10 @@
+package com.webcheckers.model;
+
+public class Player {
+    //TODO
+    private String name;
+
+    public Player(String name) {
+        this.name = name;
+    }
+}
