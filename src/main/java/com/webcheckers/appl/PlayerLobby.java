7fa7f8b662f66260e4d0ade901387467097b3134
Juna <jonathaneid98@gmail.com>
2017-10-12 15:30:59 -0400

    Finished the PlayerLobby class to include sign in and sign out functionality as well as validating whether or not the player is already signed in.

--- src/main/java/com/webcheckers/appl/PlayerLobby.java
+++ src/main/java/com/webcheckers/appl/PlayerLobby.java
@@ -1,7 +1,41 @@
 package com.webcheckers.appl;
 
+import com.webcheckers.model.Player;
+
 import java.lang.reflect.Array;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PlayerLobby {
-	//TODO Handle Sign in and sign out
+
+    private static Map<String, Player> playerMap;
+
+    public PlayerLobby(){
+        playerMap = new HashMap<>();
+    }
+
+    public boolean signInPlayer(String name){
+        if (isLoggedIn(name)){
+            return false;
+        }
+        else{
+            Player playerLogin = new Player(name);
+            playerMap.put(name, playerLogin);
+            return true;
+        }
+    }
+
+    public boolean signOutPlayer(String name){
+        if (!isLoggedIn(name)){
+            return false;
+        }
+        else{
+            playerMap.remove(name);
+            return true;
+        }
+    }
+
+    public boolean isLoggedIn(String name){
+        return playerMap.containsKey(name);
+    }
 }
