90238cdf7d8f0bf466b7d267749835814cfe23d4
Andrew Didycz <axd5449@rit.edu>
2017-10-12 18:26:42 -0400

    Fixed the bun in WebServer where there was no playerLobby argument in GetHome Route.

--- src/main/java/com/webcheckers/ui/WebServer.java
+++ src/main/java/com/webcheckers/ui/WebServer.java
@@ -140,7 +140,7 @@ public class WebServer {
 		//// code clean; using small classes.
 
 		// Shows the Checkers game Home page.
-		get(HOME_URL, new GetHomeRoute(templateEngine));
+		get(HOME_URL, new GetHomeRoute(templateEngine, playerLobby));
 
 		//Sign in Page
 		get(SIGN_IN_URL, new GetSignInRoute(templateEngine));
