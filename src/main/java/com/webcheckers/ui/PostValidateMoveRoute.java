f9e0518069ad6cf4c6e9585659817c23091619ca
dis446 <dis446@yahoo.com>
2017-10-16 11:51:19 -0400

    Add the Acceptance testing excel spreadsheet. Another attempt at getting the dumb Ajax routes to work.

--- src/main/java/com/webcheckers/ui/PostValidateMoveRoute.java
+++ src/main/java/com/webcheckers/ui/PostValidateMoveRoute.java
@@ -38,12 +38,13 @@ public class PostValidateMoveRoute implements Route {
         Board board = session.attribute("board");
         Message message;
         if (board.isValidMove(move)){
-            message = new Message("Valid Move", Message.type.info);
+            message = new Message("", Message.type.info);
         }
         else{
             message = new Message("Invalid Move", Message.type.error);
         }
-        response.body(gson.toJson(message));
+        String jsonString = gson.toJson(message, Message.class);
+        response.body(jsonString);
         response.redirect(WebServer.GAME_URL);
         halt();
         return null;
