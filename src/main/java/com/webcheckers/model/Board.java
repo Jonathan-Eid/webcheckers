bba1c7e33611b3b5ba27dddee157ead044345044
Juna <jonathaneid98@gmail.com>
2017-10-15 21:01:30 -0400

    Initialized some values i missed

--- src/main/java/com/webcheckers/model/Board.java
+++ src/main/java/com/webcheckers/model/Board.java
@@ -12,8 +12,8 @@ public class Board {
 
     public Board(){
         this.rows = new Row[7];
-        for (int y; y < 8; y++){
-            for (int x; x < 8; x++){
+        for (int y = 0; y < 8; y++){
+            for (int x = 0; x < 8; x++){
                 if (y < 3){
                     if(y % 2 == 0){
                         if (x % 2 == 0){
@@ -21,13 +21,13 @@ public class Board {
                         }
                         else{
                             rows[y].setSpace(x, Square.spotType.RED);
-                            rows[y].getSpace(x).setPiece(new Piece(Piece.colors.WHITE));
+                            rows[y].getSpace(x).setPiece(new Piece(Piece.color.WHITE));
                         }
                     }
                     else{
                         if (x % 2 == 0){
                             rows[y].setSpace(x, Square.spotType.RED);
-                            rows[y].getSpace(x).setPiece(new Piece(Piece.colors.WHITE));
+                            rows[y].getSpace(x).setPiece(new Piece(Piece.color.WHITE));
                         }
                         else{
                             rows[y].setSpace(x, Square.spotType.EMPTY_WHITE);
@@ -41,13 +41,13 @@ public class Board {
                         }
                         else{
                             rows[y].setSpace(x, Square.spotType.RED);
-                            rows[y].getSpace(x).setPiece(new Piece(Piece.colors.RED));
+                            rows[y].getSpace(x).setPiece(new Piece(Piece.color.RED));
                         }
                     }
                     else{
                         if (x % 2 == 0){
                             rows[y].setSpace(x, Square.spotType.RED);
-                            rows[y].getSpace(x).setPiece(new Piece(Piece.colors.RED));
+                            rows[y].getSpace(x).setPiece(new Piece(Piece.color.RED));
                         }
                         else{
                             rows[y].setSpace(x, Square.spotType.EMPTY_WHITE);
