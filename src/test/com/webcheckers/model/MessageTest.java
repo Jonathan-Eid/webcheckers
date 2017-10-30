3b9bb3d53285f247cee8affed642dfbed28cd020
Andrew Didycz <axd5449@rit.edu>
2017-10-29 23:57:09 -0400

    Implemented MessageTest to test for a normal message, an error message, and a message with null text.

--- src/test/com/webcheckers/model/MessageTest.java
+++ src/test/com/webcheckers/model/MessageTest.java
@@ -19,18 +19,23 @@ public class MessageTest {
     @Test
     public void regularMessage() throws Exception {
         CuT = new Message("Hello World!", Message.type.info);
-
+        assertNotNull(CuT.getText());
+        assertEquals(CuT.getType(), Message.type.info);
     }
 
     @Test
     public void nullMessage() throws Exception {
         CuT = new Message(null, Message.type.info);
+        assertNull(CuT.getText());
+        assertEquals(CuT.getType(), Message.type.info);
     }
 
 
     @Test
     public void errorMessage() throws Exception {
         CuT = new Message("Error World!", Message.type.error);
+        assertNotNull(CuT.getText());
+        assertEquals(CuT.getType(), Message.type.error);
     }
 
 }
