9ea6a7b147d796deed66aada7b9a41aa3ec88599
Andrew Didycz <axd5449@rit.edu>
2017-10-29 22:56:27 -0400

    Added functionality to GetHomeRouteTest. Now runs three test cases to test for new GetHomeRoute, GetHomeRoute with logged-in user, and GetHomeRoute with user in game.

--- src/test/com/webcheckers/ui/GetHomeRouteTest.java
+++ src/test/com/webcheckers/ui/GetHomeRouteTest.java
@@ -1,12 +1,14 @@
 package com.webcheckers.ui;
 
 import com.webcheckers.appl.PlayerLobby;
+import com.webcheckers.model.Player;
 import org.junit.Before;
 import org.junit.Test;
 import spark.*;
 
 import java.util.Map;
 
+import static com.webcheckers.ui.PostSignInRoute.PLAYER_LIST_ATTR;
 import static org.junit.Assert.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
@@ -30,6 +32,8 @@ public class GetHomeRouteTest {
     private TemplateEngine engine;
     private PlayerLobby playerLobby;
 
+    public static final String PLAYER_NAME_ATTR = "playerName";
+
 
     @Before
     public void setUp() throws Exception {
@@ -39,7 +43,8 @@ public class GetHomeRouteTest {
         when(request.session()).thenReturn(session);
         response = mock(Response.class);
         engine = mock(TemplateEngine.class);
-        playerLobby = mock(PlayerLobby.class);
+
+        playerLobby = new PlayerLobby();
 
         // create a unique CuT for each test
         CuT = new GetHomeRoute(engine, playerLobby);
@@ -51,7 +56,6 @@ public class GetHomeRouteTest {
      */
     @Test
     public void new_session() {
-        /*
         when(session.isNew()).thenReturn(true);
         final MyModelAndView myModelView = new MyModelAndView();
         when(engine.render(any(ModelAndView.class))).thenAnswer(MyModelAndView.makeAnswer(myModelView));
@@ -62,9 +66,71 @@ public class GetHomeRouteTest {
         assertTrue(model instanceof Map);
 
         final Map<String, Object> vm = (Map<String, Object>) model;
-        assertEquals(GetHomeRoute.TITLE_ATTR, vm.get(GetHomeRoute.TITLE_VAL));
-        assertEquals(Boolean.FALSE, vm.get(GetHomeRoute.PLAYER_NAME_ATTR));
-        */
+        assertNotNull(model);
+        assertNotNull(engine);
+        assertNotNull(playerLobby);
+        assertTrue(model instanceof Map);
+    }
+
+    @Test
+    public void signed_in() {
+        Player player = new Player("name");
+        when(session.attribute("player")).thenReturn(player);
+        playerLobby.signInPlayer("name");
+
+        final MyModelAndView myModelView = new MyModelAndView();
+        when(engine.render(any(ModelAndView.class))).thenAnswer(MyModelAndView.makeAnswer(myModelView));
+
+        CuT.handle(request,response);
+        final Object model = myModelView.model;
+        final Map<String, Object> vm = (Map<String, Object>)model;
+
+        assertNotNull(model);
+        assertTrue(model instanceof Map);
+        assertNotNull(engine);
+        assertNotNull(playerLobby);
+        assertEquals(playerLobby.getNumPlayers(), Integer.toString(1));
+        assertNotNull(vm.get(PostSignInRoute.PLAYER_SIGNED_IN_ATTR));
+        assertTrue((boolean)vm.get(PostSignInRoute.PLAYER_SIGNED_IN_ATTR));
+        assertNotNull(vm.get(PLAYER_NAME_ATTR));
+        assertNotNull(vm.get(PLAYER_LIST_ATTR));
+        assertEquals(vm.get(PLAYER_LIST_ATTR), playerLobby.playerList(player.getName()));
+    }
+
+    @Test
+    public void game_divert() {
+        Player player = new Player("name");
+        Player opponent = new Player("opponentName");
+        when(session.attribute("player")).thenReturn(player);
+        playerLobby.signInPlayer("name");
+        playerLobby.signInPlayer("opponent");
+        playerLobby.addToGame(player, opponent);
+        playerLobby.addToGame(opponent, player);
+
+        final MyModelAndView myModelView = new MyModelAndView();
+        when(engine.render(any(ModelAndView.class))).thenAnswer(MyModelAndView.makeAnswer(myModelView));
+
+
+        try {
+            CuT.handle(request,response);
+            final Object model = myModelView.model;
+            final Map<String, Object> vm = (Map<String, Object>)model;
+
+            assertNotNull(model);
+            assertTrue(model instanceof Map);
+            assertNotNull(engine);
+            assertNotNull(playerLobby);
+            assertEquals(playerLobby.getNumPlayers(), Integer.toString(2));
+            assertNotNull(vm.get(PostSignInRoute.PLAYER_SIGNED_IN_ATTR));
+            assertTrue((boolean)vm.get(PostSignInRoute.PLAYER_SIGNED_IN_ATTR));
+            assertNotNull(vm.get(PLAYER_NAME_ATTR));
+            assertNotNull(vm.get(PLAYER_LIST_ATTR));
+            assertEquals(vm.get(PLAYER_LIST_ATTR), playerLobby.playerList(player.getName()));
+        } catch (HaltException e) {
+            // expected
+        }
+
+        verify(response).redirect(WebServer.GAME_URL);
     }
 
 }
