b6392989da45b145cc435839455c0c83ed302d6d
Andrew Didycz <axd5449@rit.edu>
2017-10-29 23:44:55 -0400

    Added functionality to GetHomeRouteTest. Now runs three test cases to test for new GetHomeRoute, GetHomeRoute with logged-in user, and GetHomeRoute with user in game.

--- src/test/com/webcheckers/model/MessageTest.java
+++ src/test/com/webcheckers/model/MessageTest.java
@@ -9,19 +9,27 @@ import static org.junit.Assert.*;
  * Created by dis446 on 10/26/17.
  */
 public class MessageTest {
+    private Message CuT;
+
     @Before
     public void setUp() throws Exception {
 
     }
 
     @Test
-    public void getText() throws Exception {
+    public void regularMessage() throws Exception {
+        CuT = new Message("Hello World!", Message.type.info);
 
     }
 
     @Test
-    public void getType() throws Exception {
+    public void nullMessage() throws Exception {
+        CuT = new Message(null, Message.type.info);
+    }
 
+    @Test
+    public void errorMessage() throws Exception {
+        CuT = new Message("Error World!", Message.type.error);
     }
 
 }
