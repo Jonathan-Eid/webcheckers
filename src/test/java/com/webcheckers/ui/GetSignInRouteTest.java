708c63bc41e2eb2584ce26d6f205ef813f164859
Ani Williams <winterwoods11@gmail.com>
2017-10-30 00:32:53 -0400

    added an equals in position. made the tests

--- src/test/java/com/webcheckers/ui/GetSignInRouteTest.java
+++ src/test/java/com/webcheckers/ui/GetSignInRouteTest.java
@@ -2,20 +2,57 @@ package com.webcheckers.ui;
 
 import org.junit.Before;
 import org.junit.Test;
+import spark.*;
+
+import java.util.Map;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by dis446 on 10/26/17.
  */
 public class GetSignInRouteTest {
+
+    /**
+     * The component-under-test (CuT).
+     */
+    private GetSignInRoute CuT;
+
+    private Request request;
+    private Session session;
+    private Response response;
+    private TemplateEngine engine;
+
     @Before
     public void setUp() throws Exception {
+        request = mock(Request.class);
+        session = mock(Session.class);
+        when(request.session()).thenReturn(session);
+        response = mock(Response.class);
+        engine = mock(TemplateEngine.class);
 
+        // create a unique CuT for each test
+        CuT = new GetSignInRoute(engine);
     }
     @Test
     public void handle() throws Exception {
+        final MyModelAndView myModelView = new MyModelAndView();
+        when(engine.render(any(ModelAndView.class))).thenAnswer(MyModelAndView.makeAnswer(myModelView));
 
+        // Invoke the test (ignore the output)
+        CuT.handle(request, response);
+
+        // Analyze the content passed into the render method
+        //   * model is a non-null Map
+        final Object model = myModelView.model;
+        assertNotNull(model);
+        assertTrue(model instanceof Map);
+
+        final Map<String, Object> vm = (Map<String, Object>) model;
+        assertEquals(vm.get(GetSignInRoute.SIGN_IN_MESSAGE_ATTR),GetSignInRoute.SIGN_IN_MESSAGE);
     }
 
 }
