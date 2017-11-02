abd2075410c4cc403fbd98ba832a214791ce02e2
Juna <jonathaneid98@gmail.com>
2017-11-02 15:44:42 -0400

    The turn class will be used to handle undoing moves by holding a stack of boards that will be popped when an undo is requested.

--- src/main/java/com/webcheckers/model/Turn.java
+++ src/main/java/com/webcheckers/model/Turn.java
@@ -1,5 +1,7 @@
 package com.webcheckers.model;
 
+import java.util.Stack;
+
 /**
  * Created by Juna on 10/31/2017.
  */
@@ -7,10 +9,12 @@ public class Turn {
 
     private Player player;
     private Piece.color color;
+    private Stack<Board> undoBoards;
 
     public Turn(Player player, Piece.color color){
         this.player=player;
         this.color = color;
+        undoBoards = new Stack<>();
     }
 
     public Player getPlayer() {
@@ -20,4 +24,14 @@ public class Turn {
     public Piece.color getColor() {
         return color;
     }
+
+    public void pushBoards(Board board){
+        undoBoards.push(board);
+    }
+
+    public Board undo(){
+        return undoBoards.pop();
+    }
+
+
 }
