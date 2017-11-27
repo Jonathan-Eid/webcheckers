3d76853cdb19a52d58921b08f72719fa688f2612
Andrew Didycz <axd5449@rit.edu>
2017-11-27 17:50:54 -0500

    Work on PostSubmitTurnRoute continues. Functionality is beginning to resemble that of the resign branch.

--- src/main/java/com/webcheckers/ui/PostSubmitTurnRoute.java
+++ src/main/java/com/webcheckers/ui/PostSubmitTurnRoute.java
@@ -4,12 +4,14 @@ import com.google.gson.Gson;
 import com.webcheckers.appl.GameCenter;
 import com.webcheckers.model.Game;
 import com.webcheckers.appl.Message;
+import com.webcheckers.model.Player;
 import spark.Request;
 import spark.Response;
 import spark.Route;
 import spark.Session;
 
 import static com.webcheckers.ui.GetGameRoute.GAME_ATTR;
+import static com.webcheckers.ui.PostSignInRoute.USER_ATTR;
 
 /**
  * Created by Juna on 11/3/2017.
@@ -37,11 +39,17 @@ public class PostSubmitTurnRoute implements Route {
     public Object handle(Request request, Response response) throws Exception {
         Session session = request.session();
         Game game = session.attribute(GAME_ATTR);
+        Player loser = session.attribute(USER_ATTR);
+        Player winner = playerLobby.getPlayerOpponent(loser);
+        Message message;
         game.finishTurn();
         game.startTurn();
         if(game.checkGameOver()) {
             gameCenter.removeGame(game);
             session.attribute(GAME_OVER_ATTR, true);
+            if(playerLobby.isInGame(loser) && playerLobby.isInGame(winner)){
+
+            }
 
         }
 
